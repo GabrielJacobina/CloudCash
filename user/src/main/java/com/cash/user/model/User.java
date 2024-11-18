8ed53bf44f24348aa58c010be2b6630ac83133c3
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2024-11-18 16:44:26 -0300

    Creating user microservices

---
+++ user/src/main/java/com/cash/user/model/User.java
@@ -0,0 +1,14 @@
+package com.cash.user.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private double balance;
+
+}

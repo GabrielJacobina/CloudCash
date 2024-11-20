ce798487bf80b10bb1ccbde67b4b19e28c66d99c
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2024-11-20 18:27:23 -0300

    Configuring authentication with Spring security

---
+++ authentication/src/main/java/com/cash/authentication/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.cash.authentication.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+    @NotNull @NotEmpty
+    private String username;
+    @NotNull @NotEmpty
+    @ToString.Exclude
+    private String password;
+    @NotNull @NotEmpty
+    private String role;
+
+    public UserDTO(@NotNull UserDTO user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.role = user.getRole();
+    }
+}
